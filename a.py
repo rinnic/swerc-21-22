dfc56bca2b3b7487f95cfd87784db50e34a2e8fe
Nicola Rinaldi <rinaldi.nicola@outlook.it>
2022-11-01 16:38:20 +0100

    discovered input() function

--- a.py
+++ a.py
@@ -1,30 +1,20 @@
-
-def solution(lines):
-  difficulties = {1,2,3,4,5,6,7,8,9,10}
-  beauties = dict()
-  for line in lines:
-    b, d = line.split(" ")
-    b, d = int(b), int(d)
-    if d in difficulties:
-      difficulties.remove(d)
-    if d not in beauties or beauties[d] < b:
-      beauties[d] = b
-  if difficulties:
-    return "MOREPROBLEMS"
-  return sum([beauties[d] for d in beauties])
-  
-    
-
 def main():
-    with open('data/secret/2.in') as file: 
-      lines = [line.strip() for line in file.readlines()]
-      start = 0
-      t = int(lines[start])
-      for _ in range(t):
-        start = start + 1
-        end = start + int(lines[start])
-        print(solution(lines[start+1 : end+1]))
-        start = end
+  t = int(input())
+  for _ in range(t):
+    difficulties = {1,2,3,4,5,6,7,8,9,10}
+    beauties = dict()
+    n = int(input())
+    for _ in range(n):
+      b, d = [int(x) for x in input().strip().split(" ")]
+      if d in difficulties:
+        difficulties.remove(d)
+      if d not in beauties or beauties[d] < b:
+        beauties[d] = b
+    
+    if not difficulties:
+      print(sum(beauties.values()))
+    else:
+      print("MOREPROBLEMS")
         
 
 if __name__ == "__main__":
